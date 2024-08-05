1a6e1d66a05e0b9d74a336b68956f98d78df4582
Can Bozyiğit <canbozyigit@Can-MacBook-Air.local>
2024-08-05 13:40:40 +0300

    added comment line

--- vue-playlist/src/App.vue
+++ vue-playlist/src/App.vue
@@ -1,41 +1,59 @@
 <template>
- 
-  <app-header v-bind:title="title"></app-header>
+  <!-- 'app-header' bileşenini kullanarak başlık kısmını oluşturur.
+       title prop'u ile başlığı aktarır ve basligiDegistir olayını dinler -->
+  <app-header v-bind:title="title" v-on:basligiDegistir="updateTitle($event)"></app-header>
+  
+  <!-- 'app-article' bileşenini kullanarak kullanıcıları görüntüler.
+       users prop'u ile kullanıcı listesini aktarır -->
   <app-article v-bind:users="users"></app-article>
+  
+  <!-- 'app-footer' bileşenini kullanarak alt bilgi kısmını oluşturur -->
   <app-footer></app-footer>
 </template>
 
 <script>
-import Header from './components/header.vue'
-import Article from './components/article.vue'
-import Footer from './components/footer.vue'
+  // Gerekli bileşenleri içe aktarır
+  import Header from './components/header.vue'
+  import Article from './components/article.vue'
+  import Footer from './components/footer.vue'
 
-export default {
-  components: {
-    'app-header': Header,
-    'app-footer':Footer,
-    'app-article':Article
-  },
-  data (){
-    return {
-      title:"Sayfanın başlığı",
-      users: [
-             {name: "liva", spec: "kale", show: false},
-             {name: "ferdi", spec: "bek", show: false},
-             {name: "maxim", spec: "solkanat", show: false},
-             {name: "irfan", spec: "sağkanat", show: false},
-             {name: "djiku", spec: "stoper", show: false},
-             {name: "en-nesyri", spec: "santrafor", show: false},
-         ]
+  export default {
+    // Bileşenleri tanımlar
+    components: {
+      'app-header': Header,
+      'app-footer': Footer,
+      'app-article': Article
+    },
+    // Bileşenin verilerini tanımlar
+    data() {
+      return {
+        // Başlık bilgisi
+        title: "Webtangular",
+        // Kullanıcı listesi
+        users: [
+          { name: "liva", spec: "kale", show: false },
+          { name: "ferdi", spec: "bek", show: false },
+          { name: "maxim", spec: "solkanat", show: false },
+          { name: "irfan", spec: "sağkanat", show: false },
+          { name: "djiku", spec: "stoper", show: false },
+          { name: "en-nesyri", spec: "santrafor", show: false }
+        ]
+      }
+    },
+    // Bileşenin metotlarını tanımlar
+    methods: {
+      // updateTitle metodu, başlık değiştirildiğinde çalışır
+      updateTitle: function(degisenbaslik) {
+        // Yeni başlığı title değişkenine atar
+        this.title = degisenbaslik;
+      }
     }
   }
-}
 </script>
+
 <style>
-h1{
-    color:black
-
-}
+  /* Tüm h1 elemanlarının yazı rengini siyah yapar */
+  h1 {
+    color: black;
+  }
 </style>
-
-
--- vue-playlist/src/components/header.vue
+++ vue-playlist/src/components/header.vue
@@ -1,33 +1,42 @@
 <template>
-    <header>
-    <h1>{{ title }}</h1>
-    </header>
+  <!-- header elemanı, bileşenin başlık kısmını içerir -->
+  <header>
+    <!-- h1 elemanı, başlığı görüntüler ve üzerine tıklanınca changeTitle metodunu çağırır -->
+    <h1 v-on:click="changeTitle">{{ title }}</h1>
+  </header>
 </template>
+
 <script>
-export default{
+export default {
+  // props, bileşene dışarıdan veri aktarımı sağlar
   props: {
-    title:{
-      type:String,
-      required:true
+    // title, String tipinde ve gerekli bir prop olarak tanımlanmış
+    title: {
+      type: String,
+      required: true
     }
   },
-  data(){
-    return{
-        
-      
+  methods: {
+    // changeTitle metodu, tıklama olayı gerçekleştiğinde çalışır
+    changeTitle: function() {
+      // bu metot, 'basligiDegistir' adında bir custom event (özel olay) yayar
+      // ve bu olayla birlikte bir mesaj iletir
+      this.$emit("basligiDegistir", "Alt bileşen tarafından değiştirildi");
     }
   }
-}
-
+};
 </script>
-<style scoped>
-header{
-    background:black;
-    padding: 10px;
-}
-h1{
-    color:#f8f3f3;
-    text-align: center;
 
+<style scoped>
+/* header elemanının arka plan rengini ve padding ayarlarını belirler */
+header {
+  background: black;
+  padding: 10px;
+}
+
+/* h1 elemanının yazı rengini ve hizalamasını belirler */
+h1 {
+  color: #f8f3f3;
+  text-align: center;
 }
 </style>
